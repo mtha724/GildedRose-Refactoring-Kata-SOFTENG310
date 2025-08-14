05b4c15437be4d103c3c8851f5205fb68ebff248
May Thazin <mtha724@aucklanduni.ac.nz>
2025-08-14 14:45:25 +1200

    feat: refactored if statements into switch case basis

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,53 +9,49 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+            // Update the quality and sellIn values for each item
+            switch (items[i].name) {
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                case "Aged Brie":
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        items[i].quality++;
                     }
-                }
+
+                    items[i].sellIn--;
+
+                    if (items[i].sellIn < 0 && items[i].quality < 50) {
+                        items[i].quality++;
+                    }
+
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (items[i].quality < 50) {
+                        items[i].quality++;
+
+                        if (items[i].sellIn < 11) {
+                                items[i].quality++;
+                        }
+                        if (items[i].sellIn < 6) {
+                                items[i].quality++;
+                        }
+                    }
+
+                    items[i].sellIn--;
+
+                    if (items[i].sellIn < 0) {
+                        items[i].quality--;
+                    }
+
+                    break;
+                default:
+                    // All other items
+                    if (items[i].quality > 0) {
+                        items[i].quality--;
+                    }
+
+                    items[i].sellIn--;
+
             }
         }
     }
